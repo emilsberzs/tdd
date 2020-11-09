fcb34ec9bfdabdcff496da9cf6c460e56703429c
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2020-11-09 02:24:15 +0000

    Front page HTML now generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -37,12 +37,12 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.broswer.find_element_by_id('id_list_table')
+        table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            'New to-do item doesnt appear in table'
         )
-
         # There is still a text box inviting her to add another to-do.
         #  She enters "Use feathers to create fly."
         self.fail('Finish the test!')
