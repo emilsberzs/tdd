1d1a6347357fde5a2398046b1345086db4ce1a65
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2020-11-23 18:50:44 +0000

    use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,41 +1,45 @@
 from selenium.webdriver.common.keys import Keys
-from unittest import skip
 from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
-        # Edith accidentally tries to submit empty list item,
-        # she hits Enter on the empty input box
+        # Edith goes to the home page and accidentally tries to submit
+        # an empty list item. She hits Enter on the empty input box
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # The home page refreshes, and there is error message stating
-        # that list item cannot be empty
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector(
-                '.form-group-has-error').text,
-            "You can't have an empty list item"
+        # The browser intercepts the request, and does not load the
+        # list page
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:invalid'
         ))
 
-        # She tries again with some text for the item and now it works
-        self.browser.find_element_by_id('id_text').send_keys('Buy milk')
-        self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
+        # She starts typing some text for the new item and the error disappears
+        self.get_item_input_box().send_keys('Buy milk')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:valid'
+        ))
+
+        # And she can submit it successfully
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
 
-        # She now decides to submit second blank list item
-        self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
+        # Perversely, she now decides to submit a second blank list item
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # She receives similiar warning
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector(
-                '.form-group-has-error').text,
-            "You can't have an empty list item"
+        # Again, the browser will not comply
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:invalid'
         ))
 
-        # And she can correct it by entering some text in
-        self.browser.find_element_by_id('id_text').send_keys('Make tea')
-        self.browser.find_element_by_id('id_text').send_keys(Keys.ENTER)
+        # And she can correct it by filling some text in
+        self.get_item_input_box().send_keys('Make tea')
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector(
+            '#id_text:valid'
+        ))
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
