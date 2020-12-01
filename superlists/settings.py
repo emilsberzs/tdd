9261302cc2e7328325e0389064eacf1fefdc3d6f
emilsberzs <70856334+emilsberzs@users.noreply.github.com>
2020-12-01 21:03:52 +0000

    email pw hidden

--- superlists/settings.py
+++ superlists/settings.py
@@ -145,6 +145,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'emiilsberzs@gmail.com'
-EMAIL_HOST_PASSWORD = 'Trewas93'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
